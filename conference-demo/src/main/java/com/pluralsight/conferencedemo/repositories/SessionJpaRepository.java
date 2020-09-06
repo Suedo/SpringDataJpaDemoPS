b6f5b58709090caec4ab165b7ba82c20cbe71332
snag <somjitnag192@gmail.com>
2020-09-06 18:07:28 +0530

    Intro: Query DSL

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
@@ -3,5 +3,14 @@ package com.pluralsight.conferencedemo.repositories;
 import com.pluralsight.conferencedemo.models.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SessionJpaRepository extends JpaRepository<Session, Long> {
+    /* uses Query DSL
+        Replaces the below query:
+        entityManager
+                .createQuery("select s from Session s where s.sessionName like :name")
+                .setParameter("name", "%" + name + "%").getResultList();
+     */
+    List<Session> findBySessionNameContains(String name);
 }
